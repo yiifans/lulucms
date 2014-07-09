e698f5e24304d29d086dad86a5d8318be3f405a9
yiifans <yiifans@sina.com>
2014-07-10 01:26:35 +0800

    update

---
+++ modules/console/ConsoleModule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\backend;
+
+class BackendModule extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\backend\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ modules/frontend/FrontendModule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\frontend;
+
+class FrontendModule extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\frontend\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
